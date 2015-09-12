3738f093c7e1076be8bcd96f2cb215cc988fb85c
dfernandezii <dfernandezii@hotmail.com>
2015-09-11 20:54:14 -0400

    first commit

---
+++ hello.rb
@@ -0,0 +1,13 @@
+the_weather = "sunny"
+case the_weather
+when "cloudy"
+	puts "Looks like it might rain"
+when "sunny"
+	puts "Break out the sunglasses... this is my kind of weather"
+when "foggy"
+	puts "Keep a safe distance when driving"
+when "rain"
+	puts "Don't forget your umbrella"
+	else
+	puts "Better bring an umbrella and sunglasses just in case. You never know!"	
+end
